368ccba7a1e9419a5fbe530634874db69fc0bb43
thosijulio <thosijulio@gmail.com>
2024-01-02 13:49:30 -0300

    FEAT - adicao de mais alguns exemplos

--- language.py
+++ language.py
@@ -10,10 +10,10 @@ api_key = os.environ.get('TMDB_API_KEY')
 print(api_key)
 
 # Movie title to search for
-movie_title = "Citizen Kane"  # Replace this with the movie title you're interested in
+movie_title = "Matrix"  # Replace this with the movie title you're interested in
 
 # TMDb API URL for searching movies
-search_url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_title}&language=pt-BR"
+search_url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_title}&language=pt-BR/credits"
 
 # Make the GET request to search for the movie
 response = requests.get(search_url)
@@ -27,16 +27,14 @@ if response.status_code == 200:
         movie_id = data['results'][0]['id']
 
         # Get details of the movie by its ID
-        movie_details_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}"
+        movie_details_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=pt-BR/credits"
         details_response = requests.get(movie_details_url)
 
         if details_response.status_code == 200:
             movie_details = details_response.json()
-
             # Get the Brazilian Portuguese title
-            title_pt_br = movie_details['title']
             print(movie_details)
-            print(f"The title of the movie in Brazilian Portuguese is: {title_pt_br}")
+            # print(f"The title of the movie in Brazilian Portuguese is: {title_pt_br}")
         else:
             print("Failed to fetch movie details.")
     else:
--- test.py
+++ test.py
@@ -4,37 +4,65 @@ ia = imdb.IMDb()
 
 # Retrieve a movie object
 # Search for the movie by its title
-movie_title = "Catch me if you can"  # Replace with your movie title
+movie_title = "Citizen Kane"  # Replace with your movie title
 movies = ia.search_movie(movie_title)
 movie = movies[0]
-not_working_keys = ['directors', 'countries', 'writer', 'color info', 'original airdate', 'plot', 'awards', 'quotes', 'soundtrack', 'release dates']
-working_keys = ["main"]
+not_working_keys = ['directors', 'countries', 'writer', 'color info', 'original airdate']
+working_keys = ['main', 'plot', 'awards', 'quotes', 'soundtrack', 'release dates']
 # Update and retrieve specific information
 ia.update(movie, working_keys)
 
-#print(movie['title'])
-#print(movie.get('awards'))
-#print(movie.get('quotes'))
-#print(movie.get('genres'))
-#print(movie.get('directors'))  # Check for directors key
-#print(movie.get('writers'))
-#print(movie.get('original airdate'))      
-#print(movie.get('color info'))      
-#print(movie.get('countries')) 
-#print(movie.get('writers')) 
-#print(movie.get('soundtrack')) 
-#print(movie.get('release dates'))
-#print(movie.get('original air date'))
-#print(movie.keys())
-
 for key in movie.keys():
-    ##print(key + ':')
-    if key == 'language codes':
-        for language in movie[key]:
-            print(language)
-    if key == 'cast':
-        for item in movie[key]:
-            ##print(dir(item))
-            ##print(f"- {item['name']} as {item.currentRole} - {item.notes}")
-            break
-    
+    if (isinstance(movie[key], list)):
+        itens = []
+        for index, item in enumerate(movie[key]):
+            if (index < 5550):
+                itens.append(item)
+            else:
+                break
+        if(key in ['cast', 'director', 'writer', 'producer', 'composer', 'cinematographer', 'editor']):
+            formattedItens = ""
+            formattedItensLength = 0
+            for item in itens:
+                if 'uncredited' not in item.currentRole and 'uncredited' not in item.notes and item.get('name') != None:
+                    formattedItens += f"{item.get('name')}{f' as {item.currentRole}' if item.currentRole else ''}, "
+                    formattedItensLength += 1
+            
+            print(f"{key} (first {formattedItensLength} of {len(itens)}): " + formattedItens)
+        elif (key in ['make up', 'set decoration','casting director','editorial department','','','costume designer','special effects', 'music department', 'certificates', 'assistant director', 'production manager', 'sound crew', 'visual effects', 'akas', 'location management', 'costume department', 'stunt performer', 'visual effect', 'plot', 'synopsis', 'distributors', 'miscellaneous crew', 'art department', 'camera and electrical department', 'other companies', 'transportation department', 'script department']):
+            pass
+        elif (key in ['genres', 'runtimes', 'countries', 'country codes', 'language codes', 'color info', 'languages']):
+            print(f"{key} (first {len(itens)}): {itens}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        else:
+            print(f"{key} (first {len(itens)}): {itens}")
+            pass
+    elif (key in ['full-size cover url', 'smart long imdb canonical title', 'smart canonical title', 'long imdb canonical title', 'long imdb title', 'canonical title', 'imdbID', 'vote', 'localized title', 'cover url']):
+        pass
+    else:
+        print(f"{key}: {movie[key]}")
+
+
+
+    ##print(key)
+    ##print(movie[key])
+    ##if key == 'cast':
+     ##   for item in movie[key]:
+      ##      ##print(dir(item))
+       ##     ##print(f"- {item['name']} as {item.currentRole} - {item.notes}")
+        ##    break
