34f04bc9e4c5cc61780b3b84a026bfdd999020b2
thosijulio <thosijulio@gmail.com>
2024-01-03 17:06:11 -0300

    FIX - erro ao puxar role quando o filme é um desenho animado

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ ia = imdb.IMDb()
 
 # Retrieve a movie object
 # Search for the movie by its title
-movie_title = "Citizen Kane"  # Replace with your movie title
+movie_title = "Inception"  # Replace with your movie title
 movies = ia.search_movie(movie_title)
 movie = movies[0]
 not_working_keys = ['directors', 'countries', 'writer', 'color info', 'original airdate']
@@ -14,6 +14,7 @@ ia.update(movie, working_keys)
 
 for key in movie.keys():
     if (isinstance(movie[key], list)):
+        print("\n")
         itens = []
         for index, item in enumerate(movie[key]):
             if (index < 5550):
@@ -21,36 +22,29 @@ for key in movie.keys():
             else:
                 break
         if(key in ['cast', 'director', 'writer', 'producer', 'composer', 'cinematographer', 'editor']):
-            formattedItens = ""
-            formattedItensLength = 0
+            formattedItens = []
             for item in itens:
                 if 'uncredited' not in item.currentRole and 'uncredited' not in item.notes and item.get('name') != None:
-                    formattedItens += f"{item.get('name')}{f' as {item.currentRole}' if item.currentRole else ''}, "
-                    formattedItensLength += 1
+                    if key == 'cast' and isinstance(item.currentRole, list):
+                        character_role = str(item.currentRole)
+                        character_name = character_role.split('_')[-2] if '_' in character_role else character_role
+                        formattedItens.append({ 'name': item.get('name'), 'role': character_name, 'notes': item.notes })
+                    elif key == 'cast':
+                        character_role = str(item.currentRole)
+                        character_name = character_role.split('_')[-2] if '_' in character_role else character_role
+                        formattedItens.append({ 'name': item.get('name'), 'role': character_name, 'notes': item.notes })
+                    else:
+                        formattedItens.append({ 'name': item.get('name'), 'notes': item.notes })
+                    # f"{item.get('name')}{f' as {item.currentRole}' if item.currentRole else f' as {item.notes}' if item.notes and item.notes != key else ''}, \n"
             
-            print(f"{key} (first {formattedItensLength} of {len(itens)}): " + formattedItens)
-        elif (key in ['make up', 'set decoration','casting director','editorial department','','','costume designer','special effects', 'music department', 'certificates', 'assistant director', 'production manager', 'sound crew', 'visual effects', 'akas', 'location management', 'costume department', 'stunt performer', 'visual effect', 'plot', 'synopsis', 'distributors', 'miscellaneous crew', 'art department', 'camera and electrical department', 'other companies', 'transportation department', 'script department']):
-            pass
+            print(f"{key}: {formattedItens}")
+        ##elif (key in ['make up', 'set decoration','casting director','editorial department','','','costume designer','special effects', 'music department', 'certificates', 'assistant director', 'production manager', 'sound crew', 'visual effects', 'akas', 'location management', 'costume department', 'stunt performer', 'visual effect', 'plot', 'synopsis', 'distributors', 'miscellaneous crew', 'art department', 'camera and electrical department', 'other companies', 'transportation department', 'script department']):
+          ##  pass
         elif (key in ['genres', 'runtimes', 'countries', 'country codes', 'language codes', 'color info', 'languages']):
-            print(f"{key} (first {len(itens)}): {itens}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            print(f"{key} (first {len(itens)}): {itens} \n")
             
         else:
-            print(f"{key} (first {len(itens)}): {itens}")
+            print(f"{key} ({len(itens)}): {itens}")
             pass
     elif (key in ['full-size cover url', 'smart long imdb canonical title', 'smart canonical title', 'long imdb canonical title', 'long imdb title', 'canonical title', 'imdbID', 'vote', 'localized title', 'cover url']):
         pass
